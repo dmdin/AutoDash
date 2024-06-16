c9398d9384ac5a6614a57d941b43a66c51aecc22
ghostmodd <ghost1mod@gmail.com>
2024-06-16 20:33:24 +0300

    Таблица

--- app/src/routes/widgets/+page.svelte
+++ app/src/routes/widgets/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import Table from '$root/lib/widgets/Table/Table.svelte';
 	import { Chart } from '../../lib/charts';
 	import { ChartType } from '../../lib/charts/types';
 	import Badge from '../../lib/widgets/Badge.svelte';
@@ -40,4 +41,5 @@
 		<Badge class={i % 2 ? 'bg-primary' : 'bg-secondary'} {...badge} />
 	{/each}
 	<Chart chart={barChart} />
+  <Table table={barChart} />
 </div>
