bf46de4512aa221c3c240b54b7dcbe8bfd014bd5
Disimasa <lichko2002@mail.ru>
2024-06-16 13:47:24 +0300

    fix some merge artefact(

--- app/src/routes/dashboards/[dashId]/ui/Block.svelte
+++ app/src/routes/dashboards/[dashId]/ui/Block.svelte
@@ -1,10 +1,10 @@
 <script>
-	import { getContext, onMount, setContext } from 'svelte';
+	import { SvelteFlowProvider } from '@xyflow/svelte'
+  import { getContext, onMount, setContext } from 'svelte';
 	import { Board } from './index';
 	import { writable } from 'svelte/store';
 	import { number } from 'echarts';
 
-<<<<<<< HEAD
   export let data = {
     order: -1,
   }
@@ -20,17 +20,8 @@
   setContext('widgetsImages', widgetImages)
 </script>
 
-<div class="w-[1080px] h-[1500px] p-4 border-x">
-	<Board />
-</div>
-=======
-import { SvelteFlowProvider } from '@xyflow/svelte'
-import { Board } from './index'
-</script>
-
 <div class="w-[1080px] h-full p-4 border-x">
     <SvelteFlowProvider>
         <Board/>
     </SvelteFlowProvider>
 </div>
->>>>>>> b00ee1657ffeb9254243dd3bbff3bda0d31370e4
