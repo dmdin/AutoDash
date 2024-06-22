29d95f61216cf85c878f310874a443875385a71d
GDaniil17 <galimovdv@yandex.ru>
2024-06-23 01:07:05 +0300

    Added custom parsers for the sources

--- search/main.py
+++ search/main.py
@@ -3,12 +3,17 @@ from pydantic import BaseModel
 import urllib.parse
 import requests
 import aiohttp
+import httpx
+import random
+import ssl
 import asyncio
-from typing import List, Optional
+import os
+from dotenv import load_dotenv
+from typing import List, Optional, Dict, Any
 import xml.etree.ElementTree as ET
 from bs4 import BeautifulSoup
-from config import config
 
+load_dotenv()
 app = FastAPI()
 
 class UrlList(BaseModel):
@@ -30,25 +35,149 @@ def parse_yandex_xml(xml_data):
             results.append({"url": url, "title": cleaned_title})
     return results
 
+user_agents = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.1 Safari/605.1.15",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0",
+    # TODO: make dynamic
+]
+
+
+class URLItem(BaseModel):
+    url: str
+    return_html: Optional[bool] = False
+    extra_data: Optional[bool] = False
+
+
+class URLListItem(BaseModel):
+    urls: List[str]
+    return_html: Optional[bool] = False
+    extra_data: Optional[bool] = False
+
+
+async def fetch_html(client: httpx.AsyncClient, url: str) -> str:
+    headers = {
+        "User-Agent": random.choice(user_agents)
+    }
+    response = await client.get(url, headers=headers, follow_redirects=True)
+
+    if response.status_code != 200 or "captcha" in response.text.lower():
+        raise HTTPException(status_code=response.status_code,
+                            detail="Доступ к контенту ограничен или требуется капча")
+
+    return response.text
+
+
+def extract_comnews_body(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    for ad_block in soup.find_all('script'):
+        ad_block.decompose()
+    for ad_block in soup.find_all('blockquote', {'class': 'quote4'}):
+        ad_block.decompose()
+    main_content = soup.find('div', {'class': 'field field-text full-html field-name-body'})
+    return main_content.get_text(separator='\n').strip() if main_content else None
+
+
+def parse_rbc_article(html_content: str, return_html: bool, extra_data: bool) -> Dict[str, Any]:
+    soup = BeautifulSoup(html_content, 'html.parser')
+    article = soup.find('article', class_='article-feature-item')
+    if not article:
+        raise HTTPException(status_code=404, detail="Статья не найдена")
+    title_tag = article.find('h1', class_='article-entry-title')
+    title = title_tag.get_text(strip=True) if title_tag else "Заголовок не найден"
+    paragraphs = article.find_all('p', class_='paragraph')
+    text = ' '.join([paragraph.get_text(strip=True) for paragraph in paragraphs])
+    article_dict = {"title": title, "text": text}
+    if extra_data:
+        article_dict["extra_data"] = {
+            "num_paragraphs": len(paragraphs),
+            "first_paragraph": paragraphs[0].get_text(strip=True) if paragraphs else ""
+        }
+    if return_html:
+        article_dict["html"] = html_content
+    return article_dict
+
+
+def parse_kommersant_article(html_content: str, return_html: bool, extra_data: bool) -> Dict[str, Any]:
+    soup = BeautifulSoup(html_content, 'html.parser')
+    first_article = soup.find('article') or soup.find('div', class_='article-class') or soup.find('section',
+                                                                                                  class_='article-class')
+    if first_article:
+        title_tag = first_article.find(['h1', 'h2', 'h3'])
+        title = title_tag.get_text(strip=True) if title_tag else "Заголовок не найден"
+        paragraphs = first_article.find_all('p')
+        text = ' '.join([paragraph.get_text(strip=True) for paragraph in paragraphs])
+        article_dict = {"title": title, "text": text}
+        if extra_data:
+            article_dict["extra_data"] = {
+                "num_paragraphs": len(paragraphs),
+                "first_paragraph": paragraphs[0].get_text(strip=True) if paragraphs else ""
+            }
+        if return_html:
+            article_dict["html"] = html_content
+        return article_dict
+    else:
+        raise HTTPException(status_code=404, detail="Первый блок статьи не найден")
+
+
+def parse_tadviser_article(html_content: str, return_html: bool, extra_data: bool) -> Dict[str, Any]:
+    soup = BeautifulSoup(html_content, 'html.parser')
+    main_content = soup.find('div', {'class': 'pub_body'})
+    if main_content:
+        paragraphs = main_content.find_all('p')
+        text = ' '.join([paragraph.get_text(strip=True) for paragraph in paragraphs])
+        article_dict = {"text": text}
+        if extra_data:
+            article_dict["extra_data"] = {
+                "num_paragraphs": len(paragraphs),
+                "first_paragraph": paragraphs[0].get_text(strip=True) if paragraphs else ""
+            }
+        if return_html:
+            article_dict["html"] = html_content
+        return article_dict
+    else:
+        raise HTTPException(status_code=404, detail="Контент не найден")
+
+
+def parse_cnews_article(html_content: str, return_html: bool, extra_data: bool) -> Dict[str, Any]:
+    soup = BeautifulSoup(html_content, 'html.parser')
+    main_content = soup.find('article', class_='news_container')
+    if main_content:
+        paragraphs = main_content.find_all('p')
+        text = ' '.join([paragraph.get_text(strip=True) for paragraph in paragraphs])
+        article_dict = {"text": text}
+        if extra_data:
+            article_dict["extra_data"] = {
+                "num_paragraphs": len(paragraphs),
+                "first_paragraph": paragraphs[0].get_text(strip=True) if paragraphs else ""
+            }
+        if return_html:
+            article_dict["html"] = html_content
+        return article_dict
+    else:
+        raise HTTPException(status_code=404, detail="Контент не найден")
+
+
 async def get_page_content(url):
     try:
         REQUEST_HEADERS = {
-            'User-Agent': "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Mobile Safari/537.36",
+            'User-Agent': random.choice(user_agents),
         }
+        ssl_context = ssl.create_default_context()
+        ssl_context.check_hostname = False
+        ssl_context.verify_mode = ssl.CERT_NONE
+
         async with aiohttp.ClientSession() as session:
-            async with session.get(url, headers=REQUEST_HEADERS) as response:
+            async with session.get(url, headers=REQUEST_HEADERS, ssl=ssl_context) as response:
                 try:
                     response.raise_for_status()
                     text = await response.text()
                     soup = BeautifulSoup(text, 'html.parser')
-
-                    # Extract all text
                     texts = soup.stripped_strings
                     all_text = ' '.join(texts)
-
-                    # Extract title
                     title = soup.title.string if soup.title else 'No title found'
-
+                    if title == 'No title found' and len(all_text.rstrip()) == 0:
+                        raise HTTPException(status_code=404, detail="Контент не найден")
                     return {
                         "url": url,
                         "all_text_from_page": all_text,
@@ -57,14 +186,28 @@ async def get_page_content(url):
                 except Exception as e:
                     return {
                         "url": url,
-                        "all_text_from_page": "",
-                        "page_title": 'No title found'
+                        "error": str(e)
                     }
     except aiohttp.ClientError as e:
         return {
             "url": url,
             "error": str(e)
         }
+
+
+async def parse_article(html_content: str, domain: str, return_html: bool, extra_data: bool) -> Dict[str, Any]:
+    if "comnews" in domain:
+        return {"content": extract_comnews_body(html_content)}
+    elif "rbc" in domain:
+        return parse_rbc_article(html_content, return_html, extra_data)
+    elif "kommersant" in domain:
+        return parse_kommersant_article(html_content, return_html, extra_data)
+    elif "tadviser" in domain:
+        return parse_tadviser_article(html_content, return_html, extra_data)
+    elif "cnews" in domain:
+        return parse_cnews_article(html_content, return_html, extra_data)
+    else:
+        return await get_page_content(domain)
 
 def search_func(query: str, sources: List[str]):
     try:
@@ -73,7 +216,7 @@ def search_func(query: str, sources: List[str]):
             encoded_query = ["site:" + urllib.parse.quote(url)+f"%20{encoded_query}" for url in sources.urls]
             encoded_query = "%20|%20".join(encoded_query)
 
-        yandex_url = f"https://yandex.ru/search/xml?folderid={config.yandex.folderid}&apikey={config.yandex.apikey}&query={encoded_query}"
+        yandex_url = f"https://yandex.ru/search/xml?folderid={os.getenv('yandex_folderid')}&apikey={os.getenv('yandex_apikey')}&query={encoded_query}"
         response = requests.get(yandex_url)
         xml_data = response.text
         parsed_data = parse_yandex_xml(xml_data)
@@ -138,6 +281,43 @@ async def search_data_for_llm(query: str, sources: Optional[UrlList]):
         return extract_results
     except requests.RequestException as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+
+
+@app.post("/parse")
+async def parse_url(item: URLItem):
+    async with httpx.AsyncClient() as client:
+        try:
+            html_content = await fetch_html(client, item.url)
+        except HTTPException as e:
+            return {"url": item.url, "error": str(e.detail)}
+        domain = item.url.split('/')[2]
+        parsed_article = parse_article(html_content, domain, item.return_html, item.extra_data)
+        return parsed_article
+
+
+@app.post("/parse_multiple")
+async def parse_urls(item: URLListItem):
+    async with httpx.AsyncClient() as client:
+        tasks = [fetch_html(client, url) for url in item.urls]
+        html_contents = await asyncio.gather(*tasks, return_exceptions=True)
+
+    results = []
+    errors = []
+
+    for url, html_content in zip(item.urls, html_contents):
+        if isinstance(html_content, Exception):
+            errors.append({"url": url, "error": str(html_content)})
+        else:
+            domain = url.split('/')[2]
+            parsed_article = await parse_article(html_content, domain, item.return_html, item.extra_data)
+            if 'error' in parsed_article:
+                errors.append(parsed_article)
+            else:
+                parsed_article["url"] = url
+                results.append(parsed_article)
+
+    return {"results": results, "errors": errors}
 
 if __name__ == '__main__':
     import uvicorn
