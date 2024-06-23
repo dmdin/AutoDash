afbd30688e4ff9aaf5183e91a4bd5403b87e537d
Dmitriy Din <dmitriy1d01@gmail.com>
2024-06-23 14:45:16 +0000

    Remove print

--- ml/presentation/web/router/model_router.py
+++ ml/presentation/web/router/model_router.py
@@ -30,7 +30,6 @@ async def generate_template_websocket(websocket: WebSocket) -> None:
         raw_data = await websocket.receive_json()
         input_data = ReportTemplateGeneratorInput(**raw_data)
         async for block in report_agent.generate_template(container, input_data):
-            print('block', type(block))
             await websocket.send_json(block.dict())
     except Exception as e:  # TODO: mb smth better
         logger.debug(e)
