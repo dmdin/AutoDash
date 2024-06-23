2887865bad7178bc165c77162e3e85f168183fc2
Artyom Sosedka <sostema@gmail.com>
2024-06-23 22:47:12 +0300

    fix: SEARCH PARSER FIX UGHHH

--- ml/schemas/search_parser.py
+++ ml/schemas/search_parser.py
@@ -2,11 +2,11 @@ from .base import CamelizedBaseModel
 
 
 class SearchParsedDocumentResult(CamelizedBaseModel):
-    title: str
-    text: str
-    url: str
-    extra_data: dict[str, str]
-    html: str
+    title: str | None
+    text: str | None
+    url: str | None
+    extra_data: dict[str, str] | None
+    html: str | None
 
 
 class SearchParsedDocumentList(CamelizedBaseModel):
@@ -16,5 +16,5 @@ class SearchParsedDocumentList(CamelizedBaseModel):
     В page_title хранится имя страницы (при наличии)
     """
 
-    results: list[SearchParsedDocumentResult]
-    errors: list[dict[str, str]]
+    results: list[SearchParsedDocumentResult] | None
+    errors: list[dict[str, str]] | None
