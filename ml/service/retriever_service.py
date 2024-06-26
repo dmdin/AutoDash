b491b527198bf7398e4bb0de551dfb0671bedc20
Artyom Sosedka <sostema@gmail.com>
2024-06-27 00:40:02 +0300

    fix: tokens are redundant

--- ml/service/retriever_service.py
+++ ml/service/retriever_service.py
@@ -5,7 +5,11 @@ from langchain.retrievers import ParentDocumentRetriever
 from langchain.storage._lc_store import create_kv_docstore
 from langchain_chroma import Chroma
 from langchain_community.storage import RedisStore
-from langchain_text_splitters import CharacterTextSplitter, TextSplitter
+from langchain_text_splitters import (
+    CharacterTextSplitter,
+    RecursiveCharacterTextSplitter,
+    TextSplitter,
+)
 
 
 @dataclass
@@ -17,26 +21,35 @@ class RetrieverService:
     use_parent_splitter: bool = True
 
     def __post_init__(self):
-        self.base_child_splitter = CharacterTextSplitter(
-            chunk_size=128, chunk_overlap=16
-        )
+        self.base_child_splitter = CharacterTextSplitter(chunk_size=64, chunk_overlap=4)
         self.base_parent_splitter = CharacterTextSplitter(
-            chunk_size=512, chunk_overlap=32
+            chunk_size=256, chunk_overlap=16
         )
-        if not self.child_splitter:
-            self.child_splitter = self.base_child_splitter
+        # if not self.child_splitter:
+        # self.child_splitter = self.base_child_splitter
         # if not self.parent_splitter and self.use_parent_splitter:
-        self.parent_splitter = self.base_parent_splitter
+        # self.parent_splitter = self.base_parent_splitter
         byte_docstore = create_kv_docstore(
             self.store
         )  # required for RedisStore to be used
+
+        self.child_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+            model_name='gpt-4',
+            chunk_size=64,
+            chunk_overlap=0,
+        )
+        self.parent_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+            model_name='gpt-4',
+            chunk_size=128,
+            chunk_overlap=8,
+        )
 
         self.retriever = ParentDocumentRetriever(
             vectorstore=self.vectorstore,
             docstore=byte_docstore,
             child_splitter=self.child_splitter,
             parent_splitter=self.parent_splitter,
-            search_kwargs={'k': 4},
+            search_kwargs={'k': 2},
         )
 
     async def health(self) -> None:
