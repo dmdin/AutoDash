3d9725da8c4198c21fc20bb3fb2da78658bc6420
Dmitriy Din <dmitriy1d01@gmail.com>
2024-06-26 13:15:35 +0300

    Added email auth

--- app/src/routes/auth/index.ts
+++ app/src/routes/auth/index.ts
@@ -2,14 +2,36 @@ import { dev } from '$app/environment';
 import { SvelteKitAuth } from '@auth/sveltekit';
 import Google from '@auth/sveltekit/providers/google';
 import GitHub from '@auth/sveltekit/providers/github';
+import Nodemailer from '@auth/sveltekit/providers/nodemailer';
 import { DrizzleAdapter } from '@auth/drizzle-adapter';
 import { db } from '$repo/db';
+import {
+	EMAIL_SERVER_HOST,
+	EMAIL_SERVER_PORT,
+	EMAIL_SERVER_USER,
+	EMAIL_SERVER_PASSWORD,
+	EMAIL_FROM
+} from '$env/static/private';
 
 export const { handle, signIn, signOut } = SvelteKitAuth({
 	debug: true,
 	adapter: DrizzleAdapter(db),
 	trustHost: true,
-	providers: [GitHub, Google],
+	providers: [
+		GitHub,
+		Google,
+		Nodemailer({
+			server: {
+				host: EMAIL_SERVER_HOST,
+				port: EMAIL_SERVER_PORT,
+				auth: {
+					user: EMAIL_SERVER_USER,
+					pass: EMAIL_SERVER_PASSWORD
+				}
+			},
+			from: EMAIL_FROM
+		})
+	],
 	callbacks: {
 		async session({ session, token }) {
 			// console.log('session', session, token)
